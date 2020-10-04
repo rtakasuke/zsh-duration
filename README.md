eb751ac89e7f66b85f333be3bbd307deffaacf61
rtakasuke <rtakasuke@users.noreply.github.com>
2020-10-04 19:43:10 +0900

    Init (#1)
    * init
    
    * init
    
    * Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    init
    
    refactor
    
    refactor

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # zsh-duration
+Zsh plugin for display the command duration when exceeds the threshold.
+
+<img width="170" alt="capt" src="https://user-images.githubusercontent.com/1833985/95013194-abea4a80-0679-11eb-9710-14fd29d5fec3.png">
+
+
+## Installation
+#### Using zplug
+If you use [zplug](https://github.com/zplug/zplug), add the following to your `.zshrc`:
+```
+zplug "rtakasuke/zsh-duration"
+```
+
+#### Manual installation
+```
+git clone https://github.com/rtakasuke/zsh-duration.git
+source ./zsh-duration/duration.plugin.zsh
+```
+
+## Configuration
+You can change the display threshold with environment variables.
+(Default: 10 seconds)
+```
+export ZSH_DURATION_THRESHOLD=10
+```
---
+++ duration.plugin.zsh
@@ -0,0 +1,38 @@
+function get-threadshold {
+    if [ "$ZSH_DURATION_THRESHOLD" ]; then
+        echo "$ZSH_DURATION_THRESHOLD"
+    else
+        echo 10
+    fi
+}
+
+function duration-preexec {
+    start_unixtime=$( date +%s )
+}
+
+function duration-precmd {
+    if [ -z "$start_unixtime" ]; then
+        return 0
+    fi
+
+    local end_unixtime
+    end_unixtime=$( date +%s )
+    local diff_unixtime=$(( end_unixtime - start_unixtime ))
+
+    local threadshold
+    threadshold=$(get-threadshold)
+    if [ "$diff_unixtime" -le "$threadshold" ]; then
+        return 0
+    fi
+
+    local sec=$(( diff_unixtime % 60 ))
+    local min=$(( diff_unixtime % 3600 / 60 ))
+    local hour=$(( diff_unixtime / 3600 ))
+    printf "Duration: %d:%02d:%02d\n\n" $hour $min $sec
+
+    unset start_unixtime
+}
+
+autoload -Uz add-zsh-hook
+add-zsh-hook preexec duration-preexec
+add-zsh-hook precmd duration-precmd
